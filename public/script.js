04485dbba6a8b31dd6d557c90e02eb57b0b4d764
lucasribou <willaylucasribou@gmail.com>
2026-03-05 21:30:11 +0100

    fix fermeture photo envoyé

--- public/script.js
+++ public/script.js
@@ -1,59 +1,14 @@
-const video = document.getElementById("video")
-const canvas = document.getElementById("canvas")
-const captureBtn = document.getElementById("captureBtn")
-const nameInput = document.getElementById("nameInput")
-
-navigator.mediaDevices.getUserMedia({video:true})
-.then(stream=>{
-video.srcObject=stream
-})
-
-captureBtn.onclick = async ()=>{
-
-const ctx = canvas.getContext("2d")
-
-canvas.width = video.videoWidth
-canvas.height = video.videoHeight
-
-ctx.drawImage(video,0,0)
-
-const blob = await new Promise(resolve=>canvas.toBlob(resolve,"image/jpeg"))
-
-const form = new FormData()
-
-form.append("photo",blob)
-form.append("name",nameInput.value)
-
-await fetch("/upload",{
-method:"POST",
-body:form
-})
-
-showSuccess()
-
-}
+const successScreen = document.getElementById("successScreen")
 
 function showSuccess(){
-
-document.getElementById("successScreen").classList.remove("hidden")
-
-confetti()
-
+  successScreen.classList.remove("hidden")
+  confetti()
 }
 
 function restart(){
-
-// cacher l'écran succès
-document.getElementById("successScreen").classList.add("hidden")
-
-// relancer la caméra si besoin
-navigator.mediaDevices.getUserMedia({video:true})
-.then(stream=>{
-video.srcObject = stream
-})
-
+  successScreen.classList.add("hidden")
 }
 
 function leave(){
-window.location.href="/"
+  window.location.href="/"
 }
